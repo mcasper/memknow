340d7de76303150ffa7b1dffcc31620f76860326
Matt Casper <matthewvcasper@gmail.com>
2014-11-19 18:18:41 -0800

    add guest user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user = User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) || GuestUser.new
   end
 
   def sign_in(user)
