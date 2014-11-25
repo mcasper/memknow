946fe7e880faa92e8709e3acf5be135d1f112653
Matt Casper <matthewvcasper@gmail.com>
2014-11-24 18:18:07 -0800

    allow for nil user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) || GuestUser.new
+    @current_user ||= User.find_by(id: session[:user_id]) || GuestUser.new
   end
 
   def sign_in(user)
