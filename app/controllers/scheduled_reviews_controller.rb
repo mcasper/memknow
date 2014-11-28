744fa4713913242b44189b8379ce255288336bc4
Viet Truong <viettruong2194@gmail.com>
2014-11-28 15:25:41 -0800

    Fixed issue with flashcard button sometimes not working

--- app/controllers/scheduled_reviews_controller.rb
+++ app/controllers/scheduled_reviews_controller.rb
@@ -7,4 +7,13 @@ class ScheduledReviewsController < ApplicationController
     @scheduled_review = current_user.scheduled_reviews.find(params[:id])
     @reviews = @scheduled_review.flashcards
   end
+
+  def next
+    @scheduled_review = current_user.scheduled_review.where("id > ?", @scheduled_review.flashcard.id).first
+  end
+
+  def previous
+  	@scheduled_review = @reviews.where("id < ?", @reviews.id).last
+  end
+
 end
