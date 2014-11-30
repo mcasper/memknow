40f2d7ca579f8bcbb2c096702419f70bfd7a6624
Matt Casper <matthewvcasper@gmail.com>
2014-11-29 20:18:40 -0800

    fix reviews create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   def create
-    @review = current_flashcard.reviews.new(review_params.merge(review_date: Date.today))
+    @review = current_flashcard.reviews.new(review_date: Date.today, score: submitted_score)
 
     if @review.save
       interval = @review.flashcard.calculate_next_review
       @review.flashcard.schedule_next_review(interval, current_user)
 
-      head :ok
+      redirect_to scheduled_reviews_path
     else
       render :new
     end
@@ -14,11 +14,24 @@ class ReviewsController < ApplicationController
 
   private
 
+  def submitted_score
+    case params[:commit]
+    when ":("
+      1
+    when "Ehhh"
+      2
+    when "Not bad"
+      3
+    when "Aced"
+      4
+    end
+  end
+
   def current_flashcard
     current_user.flashcards.find(params[:flashcard_id])
   end
 
-  def review_params
-    params.require(:review).permit(:score)
+  def current_scheduled_review
+    current_flashcard.scheduled_review
   end
 end
