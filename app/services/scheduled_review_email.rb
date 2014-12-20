8df14da3fa3779109c8425747dfd7c12b15456a1
Matt Casper <matthewvcasper@gmail.com>
2014-12-19 20:55:25 -0800

    heroku config env vars

--- app/services/scheduled_review_email.rb
+++ app/services/scheduled_review_email.rb
@@ -1,15 +1,12 @@
 class ScheduledReviewEmail
   attr_accessor :scheduled_review
 
-  API_KEY = "key-5c882c3bef7fb13442232a587543ce20"
-  API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/cattmasper.com"
-
   def initialize(scheduled_review)
     @scheduled_review = scheduled_review
   end
 
   def next_review
-    RestClient.post API_URL+"/messages",
+    RestClient.post ENV["API_URL"] + "/messages",
       from: "study@cattmasper.com",
       to: "#{scheduled_review.user.email}",
       subject: "Stop! Study time",
--- app/services/welcome_email.rb
+++ app/services/welcome_email.rb
@@ -1,15 +1,12 @@
 class WelcomeEmail
   attr_accessor :user
 
-  API_KEY = "key-5c882c3bef7fb13442232a587543ce20"
-  API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/cattmasper.com"
-
   def initialize(user)
     @user = user
   end
 
   def welcome
-    RestClient.post API_URL+"/messages",
+    RestClient.post ENV["API_URL"] + "/messages",
       from: "study@cattmasper.com",
       to: "#{user.email}",
       subject: "You + Memknow = Genius",
